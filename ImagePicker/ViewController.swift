1d731b91526ab3feb006b82476b0e95ce6107b41
Quickzig <brian.quick@gmail.com>
2015-07-22 19:15:14 -0500

    Initial Upload

---
+++ ImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Brian Quick on 7/22/15.
+//  Copyright (c) 2015 Brian Quick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
